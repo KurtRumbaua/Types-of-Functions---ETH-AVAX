568763fa445c95465e4b44b102f692d60c77d742
KurtRumbaua <kurtrumbaua28@gmail.com>
2024-07-13 19:49:42 +0800

    final draft

---
+++ Types and Functions.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract GoCoin is ERC20, Ownable {
+    constructor(string memory name, string memory symbol, uint256 initialSupply, address initialOwner)
+        ERC20(name, symbol)
+        Ownable(initialOwner) // Pass initialOwner to Ownable constructor
+    {
+        _mint(initialOwner, initialSupply);
+    }
+
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
+
+    function burn(uint256 amount) public {
+        _burn(msg.sender, amount);
+    }
+
+    // Transfer function is inherited from ERC20.sol
+}
